5dc0c7c86c2862c4aabef8869a6016fc376775da
vukieuanh <vukieuanh.hnue@gmail.com>
2021-09-15 15:03:30 +0700

    add file

--- src/main/java/com/codegym/config/AppConfig.java
+++ src/main/java/com/codegym/config/AppConfig.java
@@ -3,12 +3,16 @@ package com.codegym.config;
 import com.codegym.service.IStudentService;
 import com.codegym.service.StudentService;
 import org.springframework.beans.BeansException;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 import org.thymeleaf.spring5.SpringTemplateEngine;
@@ -16,10 +20,16 @@ import org.thymeleaf.spring5.templateresolver.SpringResourceTemplateResolver;
 import org.thymeleaf.spring5.view.ThymeleafViewResolver;
 import org.thymeleaf.templatemode.TemplateMode;
 
+import java.io.IOException;
+
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.codegym.controller")
+@PropertySource("classpath:upload_file.properties")
 public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
+
+    @Value("${file-upload}")
+    private String fileUpload;
 
     private ApplicationContext applicationContext;
 
@@ -65,6 +75,21 @@ public class AppConfig implements WebMvcConfigurer, ApplicationContextAware {
     @Bean
     public IStudentService studentService(){
         return new StudentService();
+    }
+
+    //Cấu hình upload file
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/image/**")
+                .addResourceLocations("file:" + fileUpload);
+
+    }
+
+    @Bean(name = "multipartResolver")
+    public CommonsMultipartResolver getResolver() throws IOException {
+        CommonsMultipartResolver resolver = new CommonsMultipartResolver();
+        resolver.setMaxUploadSizePerFile(52428800);
+        return resolver;
     }
 
 }
--- src/main/java/com/codegym/model/Student.java
+++ src/main/java/com/codegym/model/Student.java
@@ -1,11 +1,29 @@
 package com.codegym.model;
 
+
 public class Student {
     private int id;
     private String name;
     private String email;
+    private String img;
 
     public Student() {
+    }
+
+
+    public String getImg() {
+        return img;
+    }
+
+    public void setImg(String img) {
+        this.img = img;
+    }
+
+    public Student(int id, String name, String email, String img) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.img = img;
     }
 
     public Student(int id, String name, String email) {
